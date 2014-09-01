64fb944c657bc70b0b5ecfa00e9c79e3b871460d
Alex Sorici <alex.sorici@gmail.com>
2014-09-01 16:26:33 +0300

    Cyclic implementation of SensorUpdateReceiver

--- src/org/aimas/ami/cmm/agent/coordinator/SensorUpdateReceiver.java
+++ src/org/aimas/ami/cmm/agent/coordinator/SensorUpdateReceiver.java
@@ -1,17 +1,18 @@
 package org.aimas.ami.cmm.agent.coordinator;
 
 import jade.content.ContentElement;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.lang.acl.MessageTemplate.MatchExpression;
-import jade.proto.states.MsgReceiver;
 
 import org.aimas.ami.cmm.agent.CMMAgent;
 import org.aimas.ami.cmm.agent.onto.AssertionUpdated;
 
-public class SensorUpdateReceiver extends MsgReceiver {
+public class SensorUpdateReceiver extends CyclicBehaviour {
     private static final long serialVersionUID = 9159640468516161996L;
 	private CtxCoord coordAgent;
+	private MessageTemplate template;
 	
 	@SuppressWarnings("serial")
     private static MessageTemplate prepareTemplate(final CtxCoord coordAgent) {
@@ -46,16 +47,30 @@ public class SensorUpdateReceiver extends MsgReceiver {
     }
 	
 	public SensorUpdateReceiver(CtxCoord coordAgent) {
-		super(coordAgent, prepareTemplate(coordAgent), MsgReceiver.INFINITE, null, null);
+		super(coordAgent);
+		
+		this.coordAgent = coordAgent;
+		this.template = prepareTemplate(coordAgent);
 	}
 	
 	@Override
-	protected void handleMessage(ACLMessage msg) {
+    public void action() {
+		ACLMessage msg = myAgent.receive(template);
+		if (msg != null) {
+			handleMessage(msg);
+		}
+	    
+    }
+	
+	private void handleMessage(ACLMessage msg) {
 		// Get the content
 		try {
-	        AssertionUpdated assertionUpdate = (AssertionUpdated)coordAgent.getContentManager().extractContent(msg);
+			//System.out.println("["+ coordAgent.getName() +"]: receving AssertionUpdate from " + msg.getSender().getName() );
+			AssertionUpdated assertionUpdate = (AssertionUpdated)coordAgent.getContentManager().extractContent(msg);
 	        coordAgent.getSensorManager().insertAssertion(assertionUpdate);
 		}
-        catch (Exception e) {}
+        catch (Exception e) {
+        	e.printStackTrace();
+        }
 	}
 }
